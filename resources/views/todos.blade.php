06668380c57a7ff738e7648564853eab20d3fe66
philcz16 <40038442+philcz16@users.noreply.github.com>
2020-04-19 13:59:29 +0100

    Restyled the application

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -12,71 +12,15 @@
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 20px;
-                color:#000;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 10px;
-                color: #000;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            
+            <div class="panel-body">
             <div class="content">
                 <form action="/create/todo" method="POST">
                     {{csrf_field()}}
                     <div class="form-group">
-               <input type="text" name="todo" class="form-control"placeholder="Enter your todo to save" style="width:100%"> </div>
+               <input type="text" name="todo" class="form-control"placeholder="Enter your todo to save" style="width:30%"> </div>
             </form>
                 <div class="title m-b-md">
                     @foreach($todos as $todo) 
@@ -87,6 +31,7 @@
                     @endforeach
                 </div>
 
+            </div>
             </div>
         </div>
     </body>
--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -11,64 +11,9 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 20px;
-                color:#000;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 10px;
-                color: #000;
-            }
-        </style>
     </head>
     <body>
+        <div class="panel-body">
         <div class="flex-center position-ref full-height">
             
             <div class="content">
@@ -78,12 +23,13 @@
                     {{csrf_field()}}
 
             <div class="form-group">
-               <input type="text" name="todo" class="form-control" value="{{ $todo->todo }}" placeholder="Enter your todo to save" style="width:100%"> 
+               <input type="text" name="todo" class="form-control" value="{{ $todo->todo }}" placeholder="Enter your todo to save" style="width:30%"> 
             </div>
         </form>
                 </div>
 
             </div>
+        </div>
         </div>
     </body>
     <script src="{{ asset('js/app.js') }}"></script>
